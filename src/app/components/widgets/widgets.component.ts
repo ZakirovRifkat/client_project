c1848e5edea1396c74b39de69aa2882488039f36
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2023-02-25 12:03:05 +0300

    add: incident layout

--- src/app/components/widgets/widgets.component.ts
+++ src/app/components/widgets/widgets.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-widgets',
@@ -45,4 +46,63 @@ export class WidgetsComponent implements OnInit {
   ngOnInit(): void {
     this.setWidgets();
   }
+
+  Highcharts = Highcharts;
+  update = false;
+  chartOptions = {
+    chart: {
+      type: 'pie',
+      width: 231,
+      height: 231,
+      spacingTop: 0,
+      spacingBottom: 0,
+      spacingRight: 0,
+      spacingLeft: 0,
+      backgroundColor: 'transparent',
+    },
+    plotOptions: {
+      pie: {
+        size: 231,
+        innerSize: 161,
+        borderWidth: 0,
+        startAngle: 180,
+        colors: ['#24C38E', '#F38B01', '#161A1D'],
+        states: {
+          hover: { enabled: false },
+          inactive: { enabled: false },
+        },
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled:false,
+        }
+      },
+    },
+
+    title: {
+      verticalAlign: 'middle' as const,
+      floating: true,
+      y: 35,
+      text: `24`,
+      style: {
+        color: 'white',
+        fontSize: '32px',
+        lineHeight: '21px',
+        textAlign: 'center',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    series: [
+      {
+        type: 'pie' as const,
+        name: 'Line 1',
+        data: [6, 3, 3],
+      },
+    ],
+    tooltip: {
+      enabled: false,
+    },
+  };
 }
